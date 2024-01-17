d6edd4cbd07a7849bdae1426a964f233d140a379
Ömer Faruk Demirözü <odemiro@corp.vodafone.es>
2024-01-17 15:28:47 +0300

    .

--- my-files/regcred.sh
+++ my-files/regcred.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+export AWS_ACCOUNT=178628758487
+export AWS_REGION=us-east-1
+
 kubectl create secret docker-registry regcred \
   --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
   --docker-username=AWS \
